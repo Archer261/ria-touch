2e7bed6d85e7925d6af20d3ecb1257900cdfd721
Joseph Archer <viewtiful.kja@gmail.com>
2023-01-09 08:53:40 -0500

    Updates

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import SubHeading from './SubHeading/SubHeading'
-import ServiceItem from './ServiceItem/ServiceItem'
-import ServicePack from './ServicePack/ServicePack'
-import FooterOverlay from './Footer/FooterOverlay'
-
-export {
-    SubHeading, ServiceItem, ServicePack, FooterOverlay
-}
--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-import images from './images'
-import data from './data'
-import videoFile from '../assets/20181029_090852.mp4'
-
-export { images, data, videoFile }
